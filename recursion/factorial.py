30210465d2bc31c355f44ad9b9c06a706b0c0149
Isaac Mwesigwa <mwesigwaisaac40@gmail.com>
2023-11-21 14:24:38 +0300

    added tail recursion

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -6,3 +6,13 @@ def factorial(n):
     return n*factorial(n-1)
   
 print(factorial(100))
+
+# tail recursion version
+def factorial_tail(n, acc=1):
+    assert n >= 0 and int(n) == n, "n must be a positive integer only"
+    if n in [0, 1]:
+        return acc
+    else:
+        return factorial_tail(n - 1, n * acc)
+
+print(factorial_tail(5))
